1aa3d04270935bffaacec259b0a831a54c175c3b
MarijaMBozic <60043521+MarijaMBozic@users.noreply.github.com>
2020-08-18 21:01:19 +0200

    Adding Models

---
+++ Nedeljni_III_Bozic_Bratic_Kosarevic/Zadatak_1/Models/Recept.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Models
+{
+    public class Recept
+    {
+        public int ReceptId { get; set; }
+        public int UserId { get; set; }
+        public int TypeId { get; set; }
+        public string ReceptName { get; set; }
+        public string ReceptType { get; set; }
+        public int PersonNumber { get; set; }
+        public string Author { get; set; }
+        public string ReceptText { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
